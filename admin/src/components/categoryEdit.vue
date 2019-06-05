f204a0acc99520465f2d8b3a7250bc70a0445c74
袁凯忻 <yuankaixin@Kisn.local>
2019-06-05 08:42:04 +0800

    update

---
+++ admin/src/components/categoryEdit.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1 class="title">{{ form.title }}</h1>
+    <el-form ref="form"
+             :model="form"
+             @submit.native.prevent
+             label-width="120px">
+      <el-form-item label="分类名字">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary"
+                   size="medium"
+                   native-type="submit"
+                   @click="onSubmit">保存</el-button>
+        <el-button @click="onBack">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        title: '新建分类'
+      }
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+    onSubmit() {
+      console.log(fetch);
+    },
+    onBack() {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  },
+}
+</script>
+<style scoped>
+.title {
+  margin-bottom: 30px;
+}
+</style>
--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 
+Vue.use(iView);
+Vue.use(ElementUI);
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
